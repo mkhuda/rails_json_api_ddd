035e3bf41d3167c63bc844697f748e5cf132b302
mkhuda <mhmmd.huda@gmail.com>
2018-05-20 21:44:33 +0700

    test user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save new user" do
+    user = User.new
+    user.fullname = "Mister"
+    user.email = "mister@gmail.com"
+    user.password = "12345"
+    user.password_confirmation = "12345"
+    assert user.save, "user new"
+  end
+
+  test "failed save new user without password confirmation" do
+    user = User.new
+    user.fullname = "Mister"
+    user.email = "mister@gmail.com"
+    user.password = "12345"
+    user.password_confirmation = ""
+    assert_not user.save, "user failed without password confirmation"
+  end
+
+  test "try authenticate user" do
+    user = User.find(1).authenticate("12345")
+    assert_not_nil user, "user succesfully authenticate"
+  end
+
+  test "failed authenticate user" do
+    user = User.find(1).authenticate("123456")
+    assert_not user, "user failed authenticate"
+  end
+
+  test "try authenticate user by email" do
+    user = User.find_by(email: "waksunari@gmail.com").authenticate("123456")
+    assert_not_nil user, "user succesfully authenticate by email"
+  end
+
 end
